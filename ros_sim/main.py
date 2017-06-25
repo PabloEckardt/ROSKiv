7d97437928e3eb95d6d7aa4c14c428a2ff50f610
pablo <pablo0168@hotmail.com>
2017-06-25 00:41:46 -0500

    adding lidar line

--- ros_sim/main.py
+++ ros_sim/main.py
@@ -4,7 +4,9 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.graphics import Line
 from random import randint
+
 from kivy.animation import Animation
 
 import math
@@ -24,12 +26,18 @@ yv = 1
 class Wall(Widget):
     pass
 
+class Barrier(Widget):
+    pass
+
 class SimCar(Widget):
+
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
+
     angle = NumericProperty(0)
 
     velocity = ReferenceListProperty(velocity_x, velocity_y)
+
 
     def move(self):
         global x
@@ -39,12 +47,14 @@ class SimCar(Widget):
         global xv, xy
         ticks += 1
         if ticks % 100 == 0:
-            #self.angle -= 10
+            self.angle -= 10
             pass
-        #self.velocity_x = math.cos((self.angle * math.pi) / 180)
-        #self.velocity_y = math.sin((self.angle * math.pi) / 180)
-        self.velocity_y = yv
-        self.velocity_x = xv
+        self.velocity_x = math.cos((self.angle * math.pi) / 180)
+        self.velocity_y = math.sin((self.angle * math.pi) / 180)
+
+        #self.velocity_y = yv
+        #self.velocity_x = xv
+
         self.pos = Vector(*self.velocity) + self.pos
 
         x = self.center_x / 1
@@ -52,37 +62,44 @@ class SimCar(Widget):
         center = self.center
 
 class Simulator(Widget): # Root Widget
-    locator_x = NumericProperty(0)
-    locator_y = NumericProperty(0)
+
+    global x, xv
+    global y, yv
+    lidar_angle = 180
+    car_x = NumericProperty(0)
+    car_y = NumericProperty(0)
 
     car = ObjectProperty(None) # Get a reference of the car object defined
                               # in the widget rules
+
+
     def start_vehicle(self):
         self.car.center = self.center
-        self.car.velocity = Vector(4, 0).rotate(randint(0, 360))
+        with self.canvas:
+            self.lidar_beam = Line(points=[0,0,0,0])
 
     def update(self, dt):
-        global x, xv
-        global y, yv
+
+
         self.car.move()
 
-        self.locator_x = x
-        self.locator_y = y
+        self.car_x = x
+        self.car_y = y
 
         if self.car.collide_widget(self.wall_left) or self.car.collide_widget(self.wall_right):
             self.car.center = self.center
-            xv *= -1
 
         if self.car.collide_widget(self.wall_top) or self.car.collide_widget(self.wall_down):
             self.car.center = self.center
-            yv *= -1
 
+        self.lidar_beam.points = [self.car.pos[0] + 25, self.car.pos[1] + 25, self.car.pos[0] + 180, self.car.pos[1] + 180]
 
 class SimApp(App):
     def build(self):
         simulator = Simulator()
         simulator.start_vehicle()
-        Clock.schedule_interval(simulator.update, 1.0/120.0)
+        #simulator.gen_beams([90,180,270])
+        Clock.schedule_interval(simulator.update, 1.0/60.0)
         return simulator
 
 
